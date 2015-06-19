4b60725af343093d0a7b251cf1c826efa5daa016
ricardotaveras <ricardo.taveras@gmail.com>
2015-06-19 09:54:45 -0400

    Quiz con buscador de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // Autoload quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
@@ -17,6 +20,11 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 // GET /author
 router.get('/author', function(req, res) {
 	res.render('author', { author: 'Ricardo Taveras' });
+});
+
+// GET Busqueda de pregunta
+router.get('/search', function(req, res) {
+	res.render('quizes/search', {});
 });
 
 module.exports = router;
