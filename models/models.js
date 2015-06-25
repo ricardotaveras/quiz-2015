b0ae956a020d72ed227436415594fe89e0f9236e
ricardotaveras <ricardo.taveras@gmail.com>
2015-06-25 12:11:48 -0400

    Relación Quiz a Comment

--- models/models.js
+++ models/models.js
@@ -27,11 +27,19 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 	}
 );
 
-// Importar la definicion de la table Quiz en quiz.js
+// Importar la definicion de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importar definición de la tabla Comment
+var comment_path = path.join(__dirname, 'comments');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // Exportar la definicion de la tabla Quiz
+exports.Comment = Comment; // Exportar la tabla Comment
 
 // sequelize.sync() crea e inicializa tabla de preguntas dn DB
 sequelize.sync().then(function() {
