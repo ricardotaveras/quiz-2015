1d7f3ab0f579a2c51d2b444ebea58d5192c27a1a
Ricardo Taveras <ricardo.taveras@gmail.com>
2015-06-26 10:27:33 -0400

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // Autoload quizId
+router.param('commentId', commentController.load); // Autoload commentId
 
 // Definició de rutas de session
 router.get('/login', sessionController.new);  // formulario login
@@ -31,7 +32,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                                        sessionController.loginRequired, commentController.publish);
 // GET /author
 router.get('/author', function(req, res) {
 	res.render('author', { author: 'Ricardo Taveras', errors: []});
